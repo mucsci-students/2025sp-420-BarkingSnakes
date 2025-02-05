08a6176bcdbe16c22b1f585beefef8a68abae674
John-Hershey <jjhersh1@millersville.edu>
2025-02-05 17:16:16 -0500

    created attributes file

---
+++ src/attribute.py
@@ -0,0 +1,18 @@
+# Filename: attribute.py
+# Authors: John Hershey
+# Date 2-5/2025
+# class for object attribute methods and design
+class Attribute:
+    # note: list elements changed
+    # Precondition:
+    #
+    # Postcondition:
+    def addAttr(classList):
+        """Adds an attribute to a class.
+
+        Returns:
+            True if attribute added to the class
+            False if classA
+        """
+        return False
+    
