f9387d21dee817bdebd8708f8bd9372366c6aa77
Kyle <nonplayercontrolled@gmail.com>
2025-02-06 12:13:04 -0500

    Added _init_ files and basic comments to help plan tests for class funtionality

---
+++ Tests/unit_tests.py
@@ -0,0 +1,22 @@
+# Filename: unit_tests.py
+# Authors: Kyle Kalbach
+# Date: 02-06-2025
+# Description: Unit Tests for uml.py
+
+#Add a valid class
+
+#Add an invalid class 
+
+#Add an existing class
+
+#Delete an existing class
+
+#Delete a non-existing class
+
+#Delete an invalid class name
+
+#Rename an existing class to a valid class name
+
+#Rename an existing class to an invalid class name
+
+#Rename Rename a class that does not exist
