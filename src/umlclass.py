b4c1b477276c613d8995802f7c64a96e55fad892
Kyle <nonplayercontrolled@gmail.com>
2025-02-07 07:55:31 -0500

    started to stub out the umlclass class

---
+++ src/umlclass.py
@@ -0,0 +1,18 @@
+# Filename: umlclass.py
+# Authors: Kyle Kalbach
+# Date: 02-07-2025
+# Description: umlclass methods
+
+# Public add
+def add_umlclass(umlclass) :
+    pass
+
+# Public delete
+def delete_umlclass(umlclass) :
+    pass
+
+# Public rename
+
+def rename_umlclass(umlclass) :
+    pass
+
