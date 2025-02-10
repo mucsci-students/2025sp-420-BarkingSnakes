e18a230caf27cd3d3db769bf967a48f43b81bc92
Kyle <nonplayercontrolled@gmail.com>
2025-02-10 08:27:24 -0500

    Working on Testing rename_class

--- tests/test_classObj.py
+++ tests/test_classObj.py
@@ -2,10 +2,12 @@
 # Authors: Kyle Kalbach, John Hershey
 # Creation Date: 02-06-2025, Last Edit Date: 02-08-2025
 # Description: Unit Tests for uml.py
-import pytest
 import os
 import sys
 import logging
+from src.umlclass import UmlClass
+# from src import errors
+
 # use below to add directory to system path
 # sys.path.append(os.path.abspath(os.path.join('..', '2025sp-420-BarkingSnakes')))
 # adds the repo directory to sys.path
@@ -14,40 +16,61 @@ rootDir = '2025sp-420-BarkingSnakes'
 if os.path.abspath('.') not in sys.path:
     sys.path.append(os.path.abspath('.'))
 # errors can be imported once the path has been added
-import src.errors as errors
 
-
-#Add a valid class
-
-#Add an invalid class 
-def testInvalidName():
-    x = None
-    #checks
+# Rename an existing class to a valid class name
+def test_rename_existing_class():
+    """"""
+    test_class = UmlClass
+    test_class.class_name = "OriginalName"
     try:
-        errors.validName("class")
-    except Exception as e:
-        logging.log(0,f"error name is {e.name}, num={e.errorNum}")
-    assert x == errors.errorList["InvalidNameError"]
+        test_class.rename_umlclass("NewName")
+    except:
+        pass
 
-    
-#Add an existing class
+    assert test_class.class_name == "NewName"
+        
+# Rename an existing class to an invalid class name
 
-#Delete an existing class
+# Rename Rename a class that does not exist
 
-#Delete a non-existing class
-#using empty class
-def testDeleteEmpty():
-    x = None
-    try:
-        errors.noClass(None)
-    except Exception as e:
-        logging.log(0,f"error name is {e.name}, num={e.errorNum}")
-        x=e
-    assert x.errorNum == errors.errorList["NullObjectError"]
-#Delete an invalid class name
+# Add a valid Attribute
 
-#Rename an existing class to a valid class name
+# Add an invalid Attribute
 
-#Rename an existing class to an invalid class name
+# Remove an existing Attribute
 
-#Rename Rename a class that does not exist
+# Remove a nonexisting Attribute
+
+
+
+
+# This code needs to move to the UML project Test
+
+# Add a valid class
+
+# Add an invalid class 
+# def testInvalidName():
+#     x = None
+#     #checks
+#     try:
+#         errors.validName("class")
+#     except Exception as e:
+#         logging.log(0,f"error name is {e.name}, num={e.errorNum}")
+#     assert x == errors.errorList["InvalidNameError"]
+
+# Add an existing class
+
+# Delete an existing class
+
+# Delete a non-existing class
+# using empty class
+# def testDeleteEmpty():
+#     x = None
+#     try:
+#         errors.noClass(None)
+#     except Exception as e:
+#         logging.log(0,f"error name is {e.name}, num={e.errorNum}")
+#         x=e
+#     assert x.errorNum == errors.errorList["NullObjectError"]
+
+# Delete an invalid class name
