d89ba53d5dd076277b189e935cf1565ff7b2aace
SWB <steven.barnes@swbdevelopment.com>
2025-03-04 23:41:59 -0500

    updates tests to use controller

--- tests/test_save_load.py
+++ tests/test_save_load.py
@@ -7,8 +7,9 @@ import os
 import pytest
 
 from src import errors
-from src.uml import UmlProject
-from src.umlcontroller import UmlApplication
+from src.umlmodel import UmlProject
+from src.umlcontroller import UmlApplication, UmlController
+from src.views.umlview_cli import UmlCliView
 
 def test_load_no_existing_file():
     """tests that an invalid file error is given when loading a fake file"""
@@ -38,10 +39,11 @@ def test_make_no_new_file():
     """test a new file was not made"""
     var = None
     try:
-        app = UmlApplication()
+        # app = UmlApplication()
+        app = UmlController(UmlCliView())
         app.new_project("test.json")
         with open("test.json", "r") as f:
-            # make sure file exists
+            # make sure file doesn't exists
             assert not f.read()
             
     except Exception as e:
@@ -76,38 +78,48 @@ def test_load_existing_file():
 
 def test_save_existing_file():
     """test that a file will be updated when saved"""
+    var = 1
     try:
-        app = UmlApplication()
+        # app = UmlApplication()
+        app = UmlController(UmlCliView())
         app.load_project("test.json")
         #adds the umlclass with name temp
-        app.active_class = "temp"
-        app.command_add_umlclass()
-        app.project.save()
+        # app.active_class = "temp"
+        app.command_add_umlclass("temp")
+        app.model.save()
+        assert "temp" in app.model.classes
+        var = 2
         #loads the file again and checks if the change was saved
         app.load_project("test.json")
-        app.command_class("temp")
+        # app.command_class("temp")
         #try to add the class again
-        app.command_add_umlclass()
+        app.command_add_umlclass("temp")
+        var = 3
     except Exception as e:
+        var = None
         assert e == errors.DuplicateClassException()
+    
+    assert not var
     
 def test_new_old_file_exists():
     """test an existing file is not overridden 
     when project with that name is made, only when saved"""
     var = None
     try:
-        app = UmlApplication()
+        # app = UmlApplication()
+        app = UmlController(UmlCliView())
         app.new_project("test.json")
         #another app to check file state
-        app2 = UmlApplication()
+        # app2 = UmlApplication()
+        app2 = UmlController(UmlCliView())
         app2.load_project("test.json")
         #assert class remains
-        assert "temp" in app2.project.classes
+        assert "temp" in app2.model.classes
         # app-level save prompts user, so until prints are handled by
         # tests - a view-implementation-solvable issue, use project save
-        app.project.save()
+        app.model.save()
         app2.load_project("test.json")
-        assert "temp" not in app2.project.classes
+        assert "temp" not in app2.model.classes
     except Exception as e:
         var = 1
         assert e == None
