4bb9beac8501d7933bcfde070a5f4dc6acf9b587
Spence1629 <Spencerhhoover@gmail.com>
2025-03-05 19:45:19 -0500

    Gui
    Added many different gui features

--- src/gui/flask_example.py
+++ src/gui/flask_example.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,4 +8,36 @@ def hello():
 
 @app.route("/classlist")
 def classlist():
-    return ["Car", "Racecar", "Spaceship", "School"]
+    return jsonify(["Car", "Racecar", "Spaceship", "School"])
+
+@app.route("/relationlist")
+def relationlist():
+    return jsonify(["Car -> Racecar", "Racecar -> Spaceship", "Spaceship -> School"])
+
+@app.route("/classdetails")
+def classdetails():
+    class_name = request.args.get('name')
+    # Replace this with actual logic to fetch class details
+    class_info = {
+        "Car": {
+            "fields": ["make", "model", "year"],
+            "methods": ["drive()", "stop()"]
+        },
+        "Racecar": {
+            "fields": ["make", "model", "year", "top_speed"],
+            "methods": ["drive()", "stop()", "race()"]
+        },
+        "Spaceship": {
+            "fields": ["name", "crew_size", "destination"],
+            "methods": ["launch()", "land()"]
+        },
+        "School": {
+            "fields": ["name", "location", "num_students"],
+            "methods": ["enroll_student()", "graduate_student()"]
+        }
+    }
+    details = class_info.get(class_name, {"fields": [], "methods": []})
+    return jsonify(details)
+
+if __name__ == '__main__':
+    app.run(debug=True)
