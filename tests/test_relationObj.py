cd5711aea8c4117656ab9d52b3f08b346b5a69f8
John-Hershey <jjhersh1@millersville.edu>
2025-03-09 23:13:31 -0400

    added some test cases to relations

--- tests/test_relationObj.py
+++ tests/test_relationObj.py
@@ -6,6 +6,7 @@ import os
 import sys
 from src.umlclass import UmlClass
 from src.umlrelationship import UmlRelationship
+from src.umlmodel import UmlProject
 from src import errors
 
 def test_circular_relation():
@@ -18,5 +19,18 @@ def test_circular_relation():
         var = 1
         assert e == None
     assert not var
+def test_add_relation_no_classes():
+    """tests that an error is raised if classes in relation don't exist"""
+    var = 1
+    try:
+        proj = UmlProject()
+        proj.add_relationship("temp", "temp2", 0)
+    except Exception as e:
+        var = None
+        assert e == errors.NoSuchObjectException()
+    assert not var
+
+
+
     
     
