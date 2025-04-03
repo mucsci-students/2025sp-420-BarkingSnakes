406b77bf812d22c70c851d8cff27ee4890455212
SWB <steven.barnes@swbdevelopment.com>
2025-04-03 01:49:52 -0400

    Adds class commands and quit command for the Controller.

---
+++ src/umlcommands/controller_commands.py
@@ -0,0 +1,33 @@
+from base_commands import UmlCommand, ControllerCommand, Callback
+
+class AddClassCommand(ControllerCommand):
+    def execute(self):
+        self.controller.add_umlclass(self.name)
+
+    @property
+    def name(self) -> str:
+        return self._args[-1]
+    
+class RenameClassCommand(ControllerCommand):
+    def execute(self):
+        self.controller.command_class(self.oldname)
+        self.controller.command_rename_umlclass(self.newname)
+    
+    @property
+    def newname(self) -> str:
+        return self._args[-1]
+    
+    @property
+    def oldname(self) -> str:
+        return self._args[-2]
+
+class QuitCommand(ControllerCommand, Callback):
+    def execute(self):
+        if self.controller.model.has_unsaved_changes:
+            # TODO - Handle prompting for unsaved changes.
+            True
+
+UMLCOMMANDS:dict[str, UmlCommand] = {
+    r"^class add ([A-Za-z][A-Za-z0-9_]*)$": AddClassCommand,
+    r"^quit$": QuitCommand
+}
