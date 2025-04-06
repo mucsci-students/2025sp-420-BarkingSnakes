04d489ccaf24668a186eb2a3e242e74c9b83a73a
John-Hershey <jjhersh1@millersville.edu>
2025-04-06 13:18:19 -0400

    test view file now raises a test exception for test files to notice and handle

--- src/views/umlview_test.py
+++ src/views/umlview_test.py
@@ -3,14 +3,16 @@ from typing import Callable
 from views.umlview import *
 
 class UmlTestView(UmlView):
-    """"""
-    DEFAULT_PROMPT = "BS-uml"
+    """Class for test view when autnatically doing pytest of input-related commands
+    """
 
     def __init__(self):
         """"""
         self._active_class = None
         self._active_method = None
         self._prompt_response = None
+        self._prompt_text = None
+        
 
     @property
     def prompt(self) -> str:
@@ -23,7 +25,8 @@ class UmlTestView(UmlView):
         return f"{prompt}> "
 
     def inform_invalid_command(self, user_response):
-        print(f"Failed: {user_response} is not a valid option.")
+        error_return = f"Failed: {user_response} is not a valid option."
+        raise errors.TestViewPromptException(error_return)
 
     def prompt_user(self, prompt:str, callback:Callable) -> bool:
         """
@@ -60,10 +63,7 @@ class UmlTestView(UmlView):
             Returns:
                 the text the user types into the terminal
         """
-        user_input = input(f"{self.prompt}{text}")
-        while user_input == '':
-            user_input = input(f"{self.prompt}{text}")
-        return user_input
+        raise errors.TestViewPromptException(text)
 
     def get_user_command(self) -> list:
         """Parses user input and returns the next command to execute."""
@@ -72,7 +72,7 @@ class UmlTestView(UmlView):
 
     def handle_exceptions(self, error_text:str):
         """"""
-        print(error_text)
+        raise errors.TestViewPromptException(error_text)
 
     def set_active_class(self, name:str):
         """"""
@@ -93,86 +93,32 @@ class UmlTestView(UmlView):
         return self._active_method
 
     def render_umlproject(self, project:UmlProjectData):
-        """"""
-        if any(project.classes):
-            print(f"Displaying {len(project.classes)} classes.")
-            for c in project.classes:
-                self.render_umlclass(c)
-        else:
-            print("No classes to display.")
+        """print nothing when using pytest"""
+        return
     
     def render_umlclass(self, umlclass:UmlClassData):
-        """"""
-        print(umlclass.name)
-        for f in umlclass.fields:
-            self.render_umlfield(f)
-        
-        for m in umlclass.methods:
-            self.render_umlmethod(m)
+        """print nothing when using pytest"""
+        return
 
     def render_umlfield(self, umlfield:UmlFieldData):
-        """"""
-        print(f"  -{umlfield.name} ({umlfield.type})")
+        """print nothing when using pytest"""
+        return
 
     def render_umlmethod(self, umlmethod:UmlMethodData):
-        """"""
-        params = ", ".join([p.name for p in umlmethod.params])
-        print(f"  +{umlmethod.name} ({params})")
+        """print nothing when using pytest"""
+        return
 
     def render_umlmethodparam(self, umlmethodparam:UmlMethodParamData):
         """"""
         return umlmethodparam.name
 
     def render_umlrelationship(self, umlrelationship:UmlRelationshipData):
-        """"""
-        print(f"<{umlrelationship.relation_type}> {umlrelationship.source} --> {umlrelationship.destination}")
+        """print nothing when using pytest"""
+        return
 
     def quit(self):
         """"""
     
     def handle_umlexception(self, uml_exception:errors.UMLException):
-        """"""
-        try:
-            raise uml_exception
-        except errors.NoActiveProjectException:
-                self.handle_exceptions("Failed: No project has been loaded.")
-        except errors.NoActiveClassException:
-            self.handle_exceptions("Failed: No active class selection.")
-        except errors.DuplicateClassException:
-            self.handle_exceptions("Failed: A class with that name already exists in this project.")
-        except errors.DuplicateFieldException:
-            self.handle_exceptions("Failed: A field with that name already exists on this class.")
-        except errors.InvalidFileException:
-            self.handle_exceptions("Failed: File must be in .json format.")
-        except errors.DuplicateRelationshipException:
-            self.handle_exceptions("Failed: This relationship already exists in this project.")
-        except errors.NoSuchObjectException as nso_e:
-            self.handle_exceptions(f"Failed: That {nso_e.object_type} does not exist.")
-        except errors.InvalidNameException:
-            self.handle_exceptions("Failed: That name contains invalid characters, or begins with a number.")
-        except errors.MethodOverloadNotExistsException:
-            self.handle_exceptions("Failed: The arity level does not exist for this method.")
-        except errors.NoActiveMethodException:
-            self.handle_exceptions("Failed: Not in a method context. Use: method help")
-        except errors.DuplicateMethodOverloadException:
-            self.handle_exceptions("Failed: An arity level already exists for the target method.")
-        except errors.MethodNameNotExistsException:
-            self.handle_exceptions("Failed: a method with that name and arity does not exist in this class")
-        except errors.FileAlreadyExistsException:
-            prompt = "Warning: A file with that name already exists.  Would you like to override the file?"
-            print(self.callback)
-            callback = self.callback
-            # self.set_callback(None)
-            self.prompt_user(prompt, callback)
-        except errors.FileHasUnsavedChangesException:
-            prompt = "Warning: The current project has unsaved changes.  Do you want to continue without saving?"
-            callback = self.callback
-            self.prompt_user(prompt, callback)
-        except errors.UMLException as uml_e:
-            self.handle_exceptions(f"Operation failed:UML Error:{uml_e}")
-        except EOFError:
-            self.is_running = False
-        except Exception as e:
-            self.handle_exceptions(f"Operation failed:Error:{e}")
-            # raise e
-            # logging.info(f" unknown error occured: {e.args}")
+        """raise error for test class to notice"""
+        raise uml_exception
