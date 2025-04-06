2ea121cc0a7d098df5f8c62df066755c20d9c342
John-Hershey <jjhersh1@millersville.edu>
2025-04-06 17:41:37 -0400

    relation class file covered

--- tests/test_relationObj.py
+++ tests/test_relationObj.py
@@ -7,6 +7,38 @@ from src.umlrelationship import UmlRelationship, RelationshipType
 from src.umlmodel import UmlProject
 from src import errors
 
+### relation class tests
+
+def test_valid_types():
+    """tests that valid types method return all valid relation types"""
+    assert UmlRelationship.valid_relation_types() == "\'aggregation\', \'composition\', \'inheritance\', \'realization\'"
+
+#test __eq__ equality
+def test_equality_different_type():
+    """tests that relationships with different types are not equal"""
+    relation1 = UmlRelationship(RelationshipType.AGGREGATION, UmlClass("temp"), UmlClass("temp1"))
+    relation2 = UmlRelationship(RelationshipType.COMPOSITION, UmlClass("temp"), UmlClass("temp1"))
+    assert relation1 != relation2
+    
+def test_equality_different_class():
+    """tests that relationships with different classes are not equal"""
+    relation1 = UmlRelationship(RelationshipType.COMPOSITION, UmlClass("temp3"), UmlClass("temp2"))
+    relation2 = UmlRelationship(RelationshipType.COMPOSITION, UmlClass("temp"), UmlClass("temp1"))
+    assert relation1 != relation2
+    
+def test_equality_same_class():
+    """tests that relationships with same type and classes are equal"""
+    relation1 = UmlRelationship(RelationshipType.AGGREGATION, UmlClass("temp"), UmlClass("temp1"))
+    relation2 = UmlRelationship(RelationshipType.AGGREGATION, UmlClass("temp"), UmlClass("temp1"))
+    assert relation1 == relation2
+    
+def test_equality_mirrored_class():
+    """tests that relationships with reversed classes are not equal"""
+    relation1 = UmlRelationship(RelationshipType.AGGREGATION, UmlClass("temp1"), UmlClass("temp1"))
+    relation2 = UmlRelationship(RelationshipType.AGGREGATION, UmlClass("temp"), UmlClass("temp"))
+    assert relation1 != relation2
+    
+### relation method tests
 def test_circular_relation():
     """tests that a circular relationship doesn't raise an error"""
     var = None
@@ -78,7 +110,7 @@ def test_relation_type_from_str_default():
     var = None
     type = None
     try:
-        type = UmlProject()._relationship_type_from_str(str(RelationshipType.DEFAULT))
+        type = UmlProject()._relationship_type_from_str("0")
     except Exception as e:
         var = 1
         assert e == errors.InvalidRelationshipTypeException()
@@ -181,15 +213,26 @@ def test_add_relation_null_type():
 def test_add_relation_no_classes():
     """tests that an error is raised if classes in relation don't exist"""
     var = None
-    relation = None
     try:
         model = UmlProject()
-        relation = model.add_relationship("temp", "temp2", "1")
+        model.add_relationship("temp", "temp2", "1")
     except Exception as e:
         var = 1
         assert e == errors.NoSuchObjectException()
     assert var
-    assert not relation
+    assert len(model.relationships) == 0
+    
+def test_add_relation_no_classes():
+    """tests that an error is raised if a class in relation doesn't exist"""
+    var = None
+    try:
+        model = UmlProject()
+        model.add_umlclass("temp")
+        model.add_relationship("temp", "temp2", "1")
+    except Exception as e:
+        var = 1
+        assert e == errors.NoSuchObjectException()
+    assert var
     assert len(model.relationships) == 0
     
 def test_add_relation():
@@ -214,12 +257,42 @@ def test_add_relation_duplicate():
         model.add_umlclass("temp")
         model.add_umlclass("temp2")
         model.add_relationship("temp", "temp2", "1")
+        model.add_relationship("temp", "temp2", "2")
+    except Exception as e:
+        var = 1
+        assert e == errors.DuplicateRelationshipException()
+    assert var
+    assert len(model.relationships) == 1
+
+def test_add_relation_duplicate_type():
+    """tests that an error is raised if the src/dest and type is a duplicate"""
+    var = None
+    try:
+        model = UmlProject()
+        model.add_umlclass("temp")
+        model.add_umlclass("temp2")
+        model.add_relationship("temp", "temp2", "1")
         model.add_relationship("temp", "temp2", "1")
     except Exception as e:
         var = 1
         assert e == errors.DuplicateRelationshipException()
     assert var
     assert len(model.relationships) == 1
+
+def test_add_relation_bidirectional():
+    """tests that an error is raised if the relation is a duplicate"""
+    var = None
+    try:
+        model = UmlProject()
+        model.add_umlclass("temp")
+        model.add_umlclass("temp2")
+        model.add_relationship("temp", "temp2", "1")
+        model.add_relationship("temp2", "temp", "2")
+    except Exception as e:
+        var = 1
+        assert e == None
+    assert not var
+    assert len(model.relationships) == 2
     
 # set_type_relation tests
 
