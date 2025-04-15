922e15496062ec13551bf5722b46a9f770668891
SWB <steven.barnes@swbdevelopment.com>
2025-04-15 02:00:25 -0400

    adds a mid point in the gui refactor.

--- src/uml_v3.py
+++ src/uml_v3.py
@@ -1,10 +1,28 @@
 from views.umlview_cli_observer import UmlViewCliObserver
+from views.umlview_gui_observer import UmlViewGuiObserver, ServerThread
+from gui.gui_v2 import app as gui
+from umlcontroller_observer import UmlControllerObserver
 
 def main():
     """"""
-    cli = UmlViewCliObserver()
-    cli.attach(cli)
-    cli.start()
+    controller = UmlControllerObserver()
+    controller.model.load("test2.json")
+    # cli = UmlViewCliObserver()
+
+    # cli.attach(controller)
+    # controller.attach(cli)
+
+    # cli.start()
+
+    # gui = setup_gui()
+    gui.attach(controller)
+    controller.attach(gui)
+    gui_thread = ServerThread(gui)
+    gui_thread.daemon = True
+    gui.set_thread(gui_thread)
+    gui_thread.start()
+
+    controller.run()
 
 if __name__ == "__main__":
     main()
