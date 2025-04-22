d8feaa868ab4e26f6fce8b7d0819f6c8ee4c9725
EvanMagill <magillep@gmail.com>
2025-04-22 13:00:22 -0400

    tweak to tests

--- src/tab_complete_test_bed.py
+++ src/tab_complete_test_bed.py
@@ -6,27 +6,47 @@ import cmd
 
 class umlShell (cmd.Cmd):
     intro = 'Welcome to Uml Shell \n'
-    prompt = 'BS-UML>'
+    prompt = 'BS-UML> '
     file = None
 
     # Basic commands
-
-    def do_class(self,args):
-        'Changes to specified class context of the form class <classname>'
-        self.prompt = args + '\n'
     
-    def do_back(self,args):
-        'Returns to the previous context'
-        self.prompt = 'BS-UML>'
+    #def do_help(self, arg):
+    #    'possibility'
+    #    print('test')
+    #    if arg == 'class':
+    #        print('hey look')
 
-    def do_a_b (self,args):
+    def help_class(self):
+        print('cool')
+
+    def help_back(self):
+        print('wow')
+
+    def do_class(self, arg):
+        'Changes to specified class context of the form class <classname>'
+        self.prompt = arg + ' > '
+    
+    def do_back(self, arg):
+        'Returns to the previous context'
+        self.prompt = 'BS-UML> '
+
+    def do_a_b (self, arg):
         'Is A space B ?'
     
-    def do_quit(self,args):
+    def do_quit(self, arg):
         'Quit the program'
         print('Exiting BS-UML')
         quit()
         return True
+    
+    def do_longCommandThatGoesLikeThis(self, arg):
+        'woop'
+        print('woop woop')
+
+    def do_longCommandThatLooksThisOtherWay(self, arg):
+        'zoop'
+        print('zoopity',arg,'doop')
     
 if __name__ == '__main__' :
     umlShell().cmdloop()
